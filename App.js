54208d1af03d2ceb322165df207e8633fa0aad3c
kuldeepmishra03642 <kuldeepmishra03642@gmail.com>
2022-07-28 22:56:40 -0700

    University Website

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
+const menu = document.querySelector('#menu')
+let menuAction = document.querySelector('.menuAction')
 
+const btn = document.querySelectorAll('body')
+
+console.log(menuAction);
+
+var counter = 1;
+
+function menuSlide() {
+    if (counter % 2 == 1) {
+        counter++;
+        menuAction.style.marginLeft = " 0vw ";
+        console.log(menuAction.style.marginLeft);
+    } else {
+        menuAction.style.marginLeft = " -100vw "
+        menuAction.style.transition = " 0.5s "
+        counter++;
+        console.log(" working ")
+    }
+}
